2a35a5cc92fecacab0f008f0c877fc452df1f088
Jakob <jakob.frenzel@hotmail.com>
2026-01-19 19:39:38 +0100

    added app content

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,134 @@
 import "./App.css";
-import { ListPlus } from "lucide-react";
+import { Info, ListPlus } from "lucide-react";
 import { Titlebar } from "./components/titlebar";
 import { Button } from "./components/ui/button";
 import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "./components/ui/empty";
+import { Input } from "./components/ui/input";
+import { Label } from "./components/ui/label";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./components/ui/accordion";
+import { Popover, PopoverContent, PopoverTrigger } from "./components/ui/popover";
+import { Field, FieldDescription, FieldError, FieldLabel } from "./components/ui/field";
+import { Checkbox } from "./components/ui/checkbox";
 
 function App() {
   return (
-    <div className="h-screen flex flex-col">
+    <div className="h-screen flex flex-col overflow-clip">
       <Titlebar/>
-      {true ? (
-        <Empty className="select-none">
-          <EmptyHeader>
-            <EmptyMedia variant="icon">
-              <ListPlus className="text-primary"/>
-            </EmptyMedia>
-            <EmptyTitle>nextbom</EmptyTitle>
-            <EmptyDescription>
-              You haven&apos;t loaded a project yet. Get started by creating or opening one.
-            </EmptyDescription>
-          </EmptyHeader>
-          <EmptyContent>
-            <div className="flex gap-2">
-              <Button>Create Project</Button>
-              <Button variant="outline">Open Project</Button>
-            </div>
-            <div className="flex flex-col items-start min-w-60 w-[30vw] pt-2">
-              <p className="text-lg font-medium pb-2">Recent</p>
-              <div className="flex flex-row">
-                <p className="text-start pr-10">filename</p>
-                <div className="text-start text-muted-foreground">6:03:47 PM [vite] (client) hmr update /src/App.tsx, /src/App.css6:03:47 PM [vite] (client) hmr update /src/App.tsx, /src/App.css6:03:47</div>
+      <div className="flex flex-col flex-1 overflow-clip">
+        {false ? (
+          <Empty className="select-none">
+            <EmptyHeader>
+              <EmptyMedia variant="icon">
+                <ListPlus className="text-primary"/>
+              </EmptyMedia>
+              <EmptyTitle>nextbom</EmptyTitle>
+              <EmptyDescription>
+                You haven&apos;t loaded a project yet. Get started by creating or opening one.
+              </EmptyDescription>
+            </EmptyHeader>
+            <EmptyContent>
+              <div className="flex gap-2">
+                <Button>Create Project</Button>
+                <Button variant="outline">Open Project</Button>
               </div>
-            </div>
-          </EmptyContent>
-        </Empty>
-      ) : (
-        <div></div>
-      )}
+              <div className="flex flex-col items-start min-w-60 w-[30vw] pt-2">
+                <p className="text-lg font-medium pb-2">Recent</p>
+                <div className="flex flex-row">
+                  <a href="#" className="text-start pr-10 text-primary font-medium">filename</a>
+                  <div className="text-start text-muted-foreground">6:03:47 PM [vite] (client) hmr update /src/App.tsx, /src/App.css6:03:47 PM [vite] (client) hmr update /src/App.tsx, /src/App.css6:03:47</div>
+                </div>
+              </div>
+            </EmptyContent>
+          </Empty>
+        ) : (
+          <div className="flex-1 overflow-y-auto px-[10vw] py-10">
+            <Accordion type="multiple" defaultValue={["item-1"]}>
+              <AccordionItem value="item-1">
+                <AccordionTrigger>1. Create a nextbom database file</AccordionTrigger>
+                <AccordionContent>
+                  <CreateNextbomFile />
+                </AccordionContent>
+              </AccordionItem>
+
+              <AccordionItem value="item-2">
+                <AccordionTrigger>2. Add part numbers</AccordionTrigger>
+                <AccordionContent>
+                  <AddPartNumbers />
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
+        )}
+      </div>
+      <div className="w-full flex items-center justify-center bg-primary">
+        <p>test</p>
+      </div>
     </div>
   );
+}
+
+function CreateNextbomFile(){
+  return (
+    <div className="flex flex-col items-start gap-6 ml-5">
+      <Field>
+        <FieldLabel>Select CSV file</FieldLabel>
+        <div className="flex flex-row items-center">
+          <Input type="file" />
+          <Popover>
+            <PopoverTrigger><Button variant={"ghost"} size={"icon-sm"} className="rounded-full ml-2"><Info /></Button></PopoverTrigger>
+            <PopoverContent className="select-none">csv file explanation</PopoverContent>
+          </Popover>
+        </div>
+      </Field>
+
+      <div className="flex flex-row justify-between w-full gap-10">
+        <Field className="min-w-60">
+          <FieldLabel>Set PCBA name</FieldLabel>
+          <div className="flex flex-row items-center">
+            <Input />
+            <div className="flex items-center gap-2 ml-4">
+              <Checkbox defaultChecked={true}/>
+              <p>Auto</p>
+            </div>
+          </div>
+        </Field>
+        
+        <Field className="min-w-60 w-auto">
+          <FieldLabel>Set BOM version</FieldLabel>
+          <div className="flex flex-row items-center">
+            <Input className="w-20"/>
+            <div className="flex items-center gap-2 ml-4">
+              <Checkbox defaultChecked={true}/>
+              <p>Auto</p>
+            </div>
+            <Popover>
+              <PopoverTrigger><Button variant={"ghost"} size={"icon-sm"} className="rounded-full ml-2"><Info /></Button></PopoverTrigger>
+              <PopoverContent className="select-none">Increase the BOM version when the design evolves over time. This field is not meant to be used to identify design variants.</PopoverContent>
+            </Popover>
+          </div>
+          <FieldError>BOM version must be a number</FieldError>
+        </Field>
+
+      </div>
+
+      
+      
+      
+
+      <p></p>
+      <p>auto (increment from project settings)</p>
+
+      
+    </div>
+  )
+}
+
+function AddPartNumbers(){
+  return (
+    <div className="grid w-full items-center gap-3">
+      test
+    </div>
+  )
 }
 
 export default App;
