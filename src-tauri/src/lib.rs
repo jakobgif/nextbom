012fde590085abdffd6d08713b3fbe7a40092932
Jakob <jakob.frenzel@hotmail.com>
2026-01-22 09:52:40 +0100

    added initial project create and close command

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,25 @@
+pub mod commands;
+pub mod data;
+
+use data::Project;
+use std::sync::Mutex;
+
+/// Application state
+pub struct AppState {
+    pub current_project: Mutex<Option<Project>>,
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .manage(AppState {
+            current_project: Mutex::new(None),
+        })
+        .invoke_handler(tauri::generate_handler![
+            commands::create_project,
+            commands::close_project,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
